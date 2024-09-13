7f51fa2f0405504729a266ad9ddeee439008a593
DanTan <alexanderlml1997@gmail.com>
2024-09-12 21:53:34 -0500

    Add SharedPreferences functionality

--- app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.example.sharedpreferences
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        val sharedPreferences = getSharedPreferences("counter_preferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        var counter = sharedPreferences.getInt("counter_value", 0)
+        counter += 1
+
+        editor.putInt("counter_value", counter)
+        editor.apply()
+
+        binding.tvCounter.text = binding.tvCounter.context.getString(R.string.counter ,counter.toString())
     }
 }
